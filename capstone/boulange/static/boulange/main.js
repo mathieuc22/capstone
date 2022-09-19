63229ac18cc339f336d4b95a0d089f8ed8280f10
Mathieu <mathieu.collet@gmail.com>
2022-09-19 11:01:57 +0200

    Reformat and document js

--- capstone/boulange/static/boulange/main.js
+++ capstone/boulange/static/boulange/main.js
@@ -1,295 +1,327 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener("DOMContentLoaded", function () {
   // If user is logged in display cart quantity
-  if (document.querySelector('.cart-quantity')) {
-    fetch('/cart', {
-      method: 'GET',
+  if (document.querySelector(".cart-quantity")) {
+    fetch("/cart", {
+      method: "GET",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-      }
+      },
     })
-    .then(response => response.json())
-    .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         updateCartQuantity(result.cart_qty);
+      });
+  }
+
+  document.addEventListener("touchstart", function () {}, false);
+
+  // For bakery owner, handle events
+  const pastryForm = document.querySelector("#pastry-form");
+  if (pastryForm) {
+    pastryForm.addEventListener("submit", addPastry);
+    // Format price on input elt
+    document.querySelector("#id_price").addEventListener("change", (event) => {
+      event.target.value = Number(event.target.value).toFixed(2);
+    });
+    // Call the delete item API
+    document.querySelectorAll(".delete-pastry").forEach((form) => {
+      form.addEventListener("click", (event) => deletePastry(event));
+    });
+  }
+  // Call the add to cart API
+  const addToCartButton = document.querySelectorAll(".addToCart-pastry");
+  if (addToCartButton) {
+    addToCartButton.forEach((form) => {
+      form.addEventListener("click", (event) => addToCartPastry(event));
     });
   }
 
-  // Récupération du nom de la page pour déclencher les bonnes fonctions
-  const path = window.location.pathname;
-  const page = path.split("/");
-  document.addEventListener("touchstart", function() {},false);
-  // Sélection des fonctions à exécuter en fonction de la page courante
-  if (page.includes("bakeries")) {
-    // Prevent submit on form and call the add API
-    const pastryForm = document.querySelector('#pastry-form')
-    if (pastryForm) {
-      pastryForm.addEventListener('submit', add_pastry);
-      document.querySelector('#id_price').addEventListener('change', (event) => { event.target.value = Number(event.target.value).toFixed(2) })
-      // Call the delete API
-      document.querySelectorAll('.delete-pastry').forEach(form => { form.addEventListener('click', (event) => delete_pastry(event)) });
-    }
-    // Call the add to cart API
-    const addToCartButton = document.querySelectorAll('.addToCart-pastry')
-    if (addToCartButton) {
-      addToCartButton.forEach(form => { form.addEventListener('click', (event) => addToCart_pastry(event)) });
-    }
-  }
-  // Call the add to cart API
-  const changeQuantity = document.querySelectorAll('.pastryQuantity')
+  // Call the change qty API and delete item API
+  const changeQuantity = document.querySelectorAll(".pastryQuantity");
   if (changeQuantity) {
-    changeQuantity.forEach(select => { select.addEventListener('change', (event) => updateQuantity(event)) });
-    document.querySelectorAll('.cart-item__delete').forEach(form => { form.addEventListener('click', (event) => delete_item(event)) });
+    changeQuantity.forEach((select) => {
+      select.addEventListener("change", (event) => updateQuantity(event));
+    });
+    document.querySelectorAll(".cart-item__delete").forEach((form) => {
+      form.addEventListener("click", (event) => deleteItem(event));
+    });
   }
+
   // Call the like API
-  const likeBakeries = document.querySelectorAll('.like-bakery')
+  const likeBakeries = document.querySelectorAll(".like-bakery");
   if (likeBakeries) {
-    likeBakeries.forEach(select => { select.addEventListener('click', (event) => like_bakery(event)) });
-  }
-  // Call the like API
-  const likeBakery = document.querySelector('#like-bakery')
-  if (likeBakery) {
-    likeBakery.addEventListener('click', (event) => like_bakery(event));
+    likeBakeries.forEach((select) => {
+      select.addEventListener("click", (event) => likeBakery(event));
+    });
   }
 });
 
-function add_pastry(event) {
-  
+/**
+ * Let the bakery owner create a pastry.
+ */
+function addPastry(event) {
   // prevent the refresh due to form submission
   event.preventDefault();
 
-  // Use the API
+  // Call API to add the pastry in the db
   fetch(window.location.pathname, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-        item: document.querySelector('#id_item').value,
-        price: document.querySelector('#id_price').value
-    })
+      item: document.querySelector("#id_item").value,
+      price: document.querySelector("#id_price").value,
+    }),
   })
-  .then(response => response.json())
-  .then(result => {
-
+    .then((response) => response.json())
+    .then((result) => {
       const listItem = document.createElement("li");
-      listItem.setAttribute("class","dishes-list__item");
+      listItem.setAttribute("class", "dishes-list__item");
       document.querySelector("#pastry-list").appendChild(listItem);
 
       const deleteItem = document.createElement("button");
-      deleteItem.setAttribute("class","dishes-list__delete-item delete-pastry");
-      deleteItem.setAttribute("id",`delete-${result.id}`);
+      deleteItem.setAttribute(
+        "class",
+        "dishes-list__delete-item delete-pastry"
+      );
+      deleteItem.setAttribute("id", `delete-${result.id}`);
       deleteItem.innerHTML = `<i class="fas fa-trash"></i>`;
       listItem.appendChild(deleteItem);
-      
+
       const contentItem = document.createElement("div");
-      contentItem.setAttribute("class","dishes-list__content");
+      contentItem.setAttribute("class", "dishes-list__content");
       listItem.appendChild(contentItem);
- 
+
       const textItem = document.createElement("div");
-      textItem.setAttribute("class","dishes-list__text");
-      textItem.innerHTML = result.message;
+      textItem.setAttribute("class", "dishes-list__text");
+      textItem.innerHTML = result.item;
       contentItem.appendChild(textItem);
       const priceItem = document.createElement("div");
-      priceItem.setAttribute("class","dishes-list__price");
+      priceItem.setAttribute("class", "dishes-list__price");
+      priceItem.innerHTML = result.price + " €";
       contentItem.appendChild(priceItem);
 
       const addToCartItem = document.createElement("button");
-      addToCartItem.setAttribute("class","dishes-list__button addToCart-pastry");
-      addToCartItem.setAttribute("id",`addToCart-${result.id}`);
+      addToCartItem.setAttribute(
+        "class",
+        "dishes-list__button addToCart-pastry"
+      );
+      addToCartItem.setAttribute("id", `addToCart-${result.id}`);
       addToCartItem.innerHTML = `<i class="fas fa-shopping-cart"></i>`;
       listItem.appendChild(addToCartItem);
 
-      addToCartItem.addEventListener('click', (event) => addToCart_pastry(event));
-      deleteItem.addEventListener('click', (event) => delete_pastry(event));
+      addToCartItem.addEventListener("click", (event) =>
+        addToCartPastry(event)
+      );
+      deleteItem.addEventListener("click", (event) => deletePastry(event));
 
-      document.querySelector('#id_item').value = '';
-      document.querySelector('#id_price').value = '';
-  });
+      document.querySelector("#id_item").value = "";
+      document.querySelector("#id_price").value = "";
+    });
 }
 
-function delete_pastry(event) {
-  
+/**
+ * Let the bakery owner delete a pastry.
+ */
+function deletePastry(event) {
   // prevent the refresh due to form submission
   event.preventDefault();
-  console.log('log' + event.currentTarget)
-  const id = event.currentTarget.id.split('delete-')[1]
-  const deleteItem = document.querySelector(`#${event.currentTarget.id}`)
 
+  const id = event.currentTarget.id.split("delete-")[1];
+  const deleteItem = document.querySelector(`#${event.currentTarget.id}`);
+
+  // Call API to delete the pastry from the db
   fetch(`/pastries/delete/${id}`, {
-    method: 'POST',
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    }
-  })
-  .then(response => response.json())
-  .then(result => {
-    // Create a message with the result
-    displayMessage(result.message);
-    deleteItem.parentElement.remove();
-  });
-
-}
-
-function addToCart_pastry(event) {
-  
-  // prevent the refresh due to form submission
-  event.preventDefault();
-  const id = event.currentTarget.id.split('addToCart-')[1]
-
-  fetch('/cart', {
-    method: 'POST',
+    method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      pastry: id
-    })
   })
-  .then(response => response.json())
-  .then(result => {
+    .then((response) => response.json())
+    .then((result) => {
       // Create a message with the result
       displayMessage(result.message);
-      updateCartQuantity(result.cart_qty);
-  });
-
+      deleteItem.parentElement.remove();
+    });
 }
 
-/*
-* Update cart quantity in the header
-*/
-function updateCartQuantity(qty) {
-  if (qty) {
-    document.querySelector('.cart-quantity').style.visibility = 'visible';
-    document.querySelector('.cart-quantity').innerHTML = qty
-  } else {
-    document.querySelector('.cart-quantity').style.visibility = 'hidden';
-  }
-
-}
-
-function updateQuantity(event) {
-  
+/**
+ * Let the user add a pastry to his shopping cart.
+ */
+function addToCartPastry(event) {
   // prevent the refresh due to form submission
   event.preventDefault();
-  const id = event.target.id.split('pastryQuantity-')[1];
-  const value = Number(event.target.value);
+  const id = event.currentTarget.id.split("addToCart-")[1];
 
-  fetch('/cart', {
-    method: 'POST',
+  // Call API to add the pastry to the db
+  fetch("/cart", {
+    method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       pastry: id,
-      quantity: value
-    })
+    }),
   })
-  .then(response => response.json())
-  .then(result => {
-      // Print result
-      console.log(result);
+    .then((response) => response.json())
+    .then((result) => {
+      // Create a message with the result
+      displayMessage(result.message);
       updateCartQuantity(result.cart_qty);
-      document.querySelector('.order-summary__price').innerHTML = `${ result.cart_total_price } €`;
-  });
-
-  const price = Number(document.querySelector(`#pastryPrice-${id}`).innerHTML.replace(" €",""));
-  document.querySelector(`#linePrice-${id}`).innerHTML = `${ (value * price).toFixed(2) } €`;
-
+    });
 }
 
-function delete_item(event) {
-  
+/*
+ * Update cart quantity in the header.
+ */
+function updateCartQuantity(qty) {
+  if (qty) {
+    document.querySelector(".cart-quantity").style.visibility = "visible";
+    document.querySelector(".cart-quantity").innerHTML = qty;
+  } else {
+    document.querySelector(".cart-quantity").style.visibility = "hidden";
+  }
+}
+
+/*
+ * Update item quantity in the cart page.
+ */
+function updateQuantity(event) {
   // prevent the refresh due to form submission
   event.preventDefault();
-  const id = event.currentTarget.id.split('delete-')[1]
-  const deleteItem = document.querySelector(`#${event.currentTarget.id}`)
-  const placeOrder = document.querySelector('#order')
-  const totalPrice = document.querySelector('.cart__price')
 
-  fetch(`/cart/delete/${id}`, {
-    method: 'DELETE',
+  const id = event.target.id.split("pastryQuantity-")[1];
+  const value = Number(event.target.value);
+
+  // Call API to add the pastry to the cart
+  fetch("/cart", {
+    method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-    }
+    },
+    body: JSON.stringify({
+      pastry: id,
+      quantity: value,
+    }),
   })
-  .then(response => response.json())
-  .then(result => {
+    .then((response) => response.json())
+    .then((result) => {
       // Print result
-      console.log(result);
-      if(result.cart_qty){
-        deleteItem.parentElement.parentElement.parentElement.remove();
-      }
-      else {
+      updateCartQuantity(result.cart_qty);
+      document.querySelector(
+        ".order-summary__price"
+      ).innerHTML = `${result.cart_total_price} €`;
+    });
 
-        const totalPrice = document.querySelector('.cart-container')
+  const price = Number(
+    document.querySelector(`#pastryPrice-${id}`).innerHTML.replace(" €", "")
+  );
+  document.querySelector(`#linePrice-${id}`).innerHTML = `${(
+    value * price
+  ).toFixed(2)} €`;
+}
+
+/*
+ * Remove an item from the shopping cart.
+ */
+function deleteItem(event) {
+  // prevent the refresh due to form submission
+  event.preventDefault();
+  const id = event.currentTarget.id.split("delete-")[1];
+  const deleteItem = document.querySelector(`#${event.currentTarget.id}`);
+  const placeOrder = document.querySelector("#order");
+  const totalPrice = document.querySelector(".cart__price");
+
+  fetch(`/cart/delete/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      // Print result
+      if (result.cart_qty) {
+        deleteItem.parentElement.parentElement.parentElement.remove();
+      } else {
+        const totalPrice = document.querySelector(".cart-container");
         placeOrder.remove();
         totalPrice.remove();
       }
       updateCartQuantity(result.cart_qty);
-      document.querySelector('.order-summary__price').innerHTML = `${ result.cart_total_price } €`;
-  });
-
+      document.querySelector(
+        ".order-summary__price"
+      ).innerHTML = `${result.cart_total_price} €`;
+    });
 }
 
-function like_bakery(event) {
-  
+/*
+ * Let the user add or remove a like for a bakery.
+ */
+function likeBakery(event) {
   // prevent the refresh due to form submission
   event.preventDefault();
   // Récupération du nom de la page pour déclencher les bonnes fonctions
   const path = window.location.pathname;
   const page = path.split("/");
 
-  const icon = event.currentTarget.querySelector('i')
-  let id
+  const icon = event.currentTarget.querySelector("i");
+  let id;
   // Sélection des fonctions à exécuter en fonction de la page courante
   if (page.includes("bakeries")) {
-    id = window.location.pathname.split("/").pop()
+    id = window.location.pathname.split("/").pop();
   } else {
-    id = event.currentTarget.id.split('like-')[1];
+    id = event.currentTarget.id.split("like-")[1];
   }
 
   // Use the API
   fetch(`/bakeries/like/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-    }
+    },
   })
-  .then(response => response.json())
-  .then(data => {
-    if (data.like) {
-      icon.classList.remove('far');
-      icon.classList.add('fas');
-      icon.classList.add('card__like-btn--active');
-      if (page.includes("bakeries")) {
-        document.querySelector("#likes").innerHTML++;
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.like) {
+        icon.classList.remove("far");
+        icon.classList.add("fas");
+        icon.classList.add("card__like-btn--active");
+        if (page.includes("bakeries")) {
+          document.querySelector("#likes").innerHTML++;
+        } else {
+          document.querySelector(`#likes-${id}`).innerHTML++;
+        }
       } else {
-        document.querySelector(`#likes-${id}`).innerHTML++;
+        icon.classList.remove("fas");
+        icon.classList.add("far");
+        icon.classList.remove("card__like-btn--active");
+        if (page.includes("bakeries")) {
+          document.querySelector("#likes").innerHTML--;
+        } else {
+          document.querySelector(`#likes-${id}`).innerHTML--;
+        }
       }
-    } else {
-      icon.classList.remove('fas');
-      icon.classList.add('far');
-      icon.classList.remove('card__like-btn--active');
-      if (page.includes("bakeries")) {
-        document.querySelector("#likes").innerHTML--;
-      } else {
-        document.querySelector(`#likes-${id}`).innerHTML--;
-      }
-    }
-  });
+    });
 }
 
+/*
+ * Utility function to display event message.
+ */
 function displayMessage(message) {
-  const messageDiv = document.createElement('div')
-  messageDiv.setAttribute("class","alert");
+  const messageDiv = document.createElement("div");
+  messageDiv.setAttribute("class", "alert");
   messageDiv.innerHTML = message;
-  document.querySelector('main').appendChild(messageDiv);
-  setTimeout(() => { document.querySelector('main').removeChild(messageDiv); }, 3000);
+  document.querySelector("main").appendChild(messageDiv);
+  setTimeout(() => {
+    document.querySelector("main").removeChild(messageDiv);
+  }, 3000);
 }
